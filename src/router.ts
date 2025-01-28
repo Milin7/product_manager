58f259e2c8707c9774ebf93d99f13d26666e6f69
Isaac Martinez <isaac_martinez.g@hotmail.com>
2025-01-28 17:16:08 -0600

    eliminate quotes

--- src/router.ts
+++ src/router.ts
@@ -12,16 +12,6 @@ router.get("/", (req, res) => {
 
 router.post(
   "/",
-
-  // body("name").notEmpty().withMessage("Product name field can't be empty"),
-  // body("price")
-  //   .isNumeric()
-  //   .withMessage("Not a valid format, you have to write a number")
-  //   .notEmpty()
-  //   .withMessage("Product price field can't be empty")
-  //   .custom(value => value > 0)
-  //   .withMessage("Value has to be higher than 0"),
-  // createProduct
   body("name").notEmpty().withMessage("Cant be empty"),
   body("price")
     .isNumeric()
