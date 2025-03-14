e602b507798090003ce30dce717f65b38ba40d1a
Isaac Martinez <isaac_martinez.g@hotmail.com>
2025-03-13 20:11:23 -0600

    Add Data router from React Router

---
+++ client/src/views/NewProduct.tsx
@@ -0,0 +1,5 @@
+export default function NewProduct(){
+    return(<>
+    <h1 className='bg-green-500'>Desde NewProduct</h1>
+    </>)
+}
---
+++ client/src/views/Products.tsx
@@ -0,0 +1,5 @@
+export default function Products(){
+    return(<>
+    <div className='bg-blue-500'>Desde Products</div>
+    </>)
+}
